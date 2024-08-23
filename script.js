8a0bd2cedfd961beee6d7984cc36d0ba1f4cbf70
Abhishan999 <148634703+Abhishan999@users.noreply.github.com>
2024-08-23 11:13:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 let slideIndex = 0;
-showSlides();
 
 function showSlides() {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
+
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
+
     slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+
+    for (let j = 0; j < dots.length; j++) {
+        dots[j].className = dots[j].className.replace(" active", "");
     }
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";  
-    dots[slideIndex - 1].className += " active";
+
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+showSlides(); // Initialize slideshow
